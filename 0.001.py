e80a233dde975028de2cff026ef12768a49e464c
Harsku <harsku1@gmail.com>
2017-02-09 13:06:43 +0100

    Add files via upload

---
+++ 0.001.py
@@ -0,0 +1,21 @@
+import csv
+pList = []
+j, i = 0, 0
+
+with open('produkter.csv', newline='') as csvfile:
+    reader = csv.reader(csvfile, delimiter=';')
+    for row in reader:
+        pList.append([])
+        for value in row:
+            pList[j].append(value)
+            i += 1
+        i = 0
+        j += 1
+k, l = 0, 0
+matching = []
+for prod in pList:
+    matching.append([s for s in pList[k] if 'Finlandia' in s])
+    if matching[k] == '':
+        matching[k].clear()
+    k += 1
+print(matching)
